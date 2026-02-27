e607ac9cd3019007fe3f9479dc7a6eb012a6128a
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-02-27 15:59:29 +0400

    Register ICrudService and CrudService

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using LibrariesWebApp.Data;
+using  LibrariesWebApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Добавление сервисов
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped(typeof(ICrudService<,>),  typeof(CrudService<,>));
 
 var app = builder.Build();
 
