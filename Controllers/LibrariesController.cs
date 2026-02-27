f892254383f6feea0ffcc2595780ee29e2b52217
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-02-27 18:43:16 +0400

    Added a method to display detailed information about the library

--- Controllers/LibrariesController.cs
+++ Controllers/LibrariesController.cs
@@ -1,8 +1,24 @@
 using LibrariesWebApp.Models;
 using LibrariesWebApp.Controllers.Base;
 using LibrariesWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibrariesWebApp.Controllers;
 
 /// <inheritdoc />
-public class LibrariesController(ICrudService<Library, int> service) : CrudController<Library, int>(service);
+public class LibrariesController(ICrudService<Library, int> service) : CrudController<Library, int>(service)
+{
+    private readonly ICrudService<Library, int> _libraryService = service;
+
+    /// <summary>
+    /// Отображает подробную информацию о библиотеке, включая список книг.
+    /// </summary>
+    /// <param name="id">Идентификатор библиотеки.</param>
+    /// <returns>Представление с данными библиотеки.</returns>
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var library = await _libraryService.GetByIdAsync(id);
+        return View(library);
+    }
+}
