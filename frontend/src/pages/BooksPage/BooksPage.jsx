7fa86e18e388b3f20e05c72875c5053f715ce19e
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-06-01 16:47:06 +0400

    fix: delete ID field from my books page for librarian

--- frontend/src/pages/BooksPage/BooksPage.jsx
+++ frontend/src/pages/BooksPage/BooksPage.jsx
@@ -64,7 +64,7 @@ export default function BooksPage() {
         {user?.role === 'Librarian' ? (
           <input
             type="text"
-            value={`Ваш уголок (ID: ${user.libraryId})`}
+            value={'Ваш уголок'}
             disabled
             style={{ flex: 1, minWidth: '200px' }}
           />
